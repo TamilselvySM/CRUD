820a23ec86dbaca6ff482f1e43504fc097fe58d5
tamilselvysm23 <tamilselvysm23@gmail.com>
2020-02-25 16:21:36 +0530

    Get list of student

--- crudOperation/src/com/controller/StudentController.java
+++ crudOperation/src/com/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,12 +9,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dao.StudentDAO;
+import com.dao.StudentDAOImpl;
+import com.entity.Student;
+
+
 public class StudentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
+    StudentDAO studentDAO = null;
+    
+    public StudentController(){
+    	studentDAO = new StudentDAOImpl();
+    }
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		List<Student> list = studentDAO.get();
+		
+		request.setAttribute("list", list);
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./views/index.jsp");
 		
 		dispatcher.forward(request, response);
